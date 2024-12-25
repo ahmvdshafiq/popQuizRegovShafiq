d5ab9f51f2bfe887cf3b489bfb32c97fee8779b2
ahmvdshafiq <ahmvdshafiq@gmail.com>
2024-12-25 20:39:58 +0000

    develop04

--- stages/dev/main.tf
+++ stages/dev/main.tf
@@ -1,5 +1,5 @@
 module "networking" {
-  source = "/home/mad/terraform/terraform/modules/networking"
+  source = "/home/mad/terraform/terraform/modules/net"
 
   cidr_block        = "10.0.0.0/16"
   public_subnet_cidr = "10.0.1.0/24"
