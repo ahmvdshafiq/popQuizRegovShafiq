518f1edbba4771085b9e8074dd57fdfb2de6b533
ahmvdshafiq <ahmvdshafiq@gmail.com>
2024-12-26 03:34:48 +0000

    develop stage

--- modules/compute/main.tf
+++ modules/compute/main.tf
@@ -3,7 +3,6 @@
 resource "aws_instance" "app_server" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  subnet_id     = var.subnet_id
   tags = {
     Name = "app-server-${var.environment}"
   }
