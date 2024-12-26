bb95b535d2a6b906539e1e3d1d606ffdb38fd436
ahmvdshafiq <ahmvdshafiq@gmail.com>
2024-12-26 05:37:47 +0000

    develop stage

--- stages/prod/outputs.tf
+++ stages/prod/outputs.tf
@@ -1,9 +1,9 @@
 output "instance_id" {
-  description = "ID of the created EC2 instance"
-  value       = aws_instance.app_server.id
+  description = "ID of the EC2 instance for dev environment"
+  value       = module.compute.instance_id
 }
 
 output "instance_public_ip" {
-  description = "Public IP of the created EC2 instance"
-  value       = aws_instance.app_server.public_ip
+  description = "Public IP of the EC2 instance for dev environment"
+  value       = module.compute.instance_public_ip
 }
